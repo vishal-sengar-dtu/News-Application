828532aa4b2c3fbf5d4007b0b60edecbe5317874
Vishal Sengar <vishalsengardtu@gmail.com>
2022-03-20 02:50:06 +0530

    Adapters

---
+++ app/src/main/java/com/vishalsengar/mynewsapplication/NewsDetailActivity.java
@@ -0,0 +1,20 @@
+package com.vishalsengar.mynewsapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewsDetailActivity extends AppCompatActivity {
+    String title, desc, content, imageUrl, url;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_detail);
+        title = getIntent().getStringExtra("title");
+        desc = getIntent().getStringExtra("desc");
+        content = getIntent().getStringExtra("content");
+        imageUrl = getIntent().getStringExtra("image");
+        url = getIntent().getStringExtra("url");
+    }
+}
--- app/src/main/java/com/vishalsengar/mynewsapplication/RetrofitAPI.java
+++ app/src/main/java/com/vishalsengar/mynewsapplication/RetrofitAPI.java
@@ -1,5 +1,6 @@
 package com.vishalsengar.mynewsapplication;
 
+import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Url;
 
